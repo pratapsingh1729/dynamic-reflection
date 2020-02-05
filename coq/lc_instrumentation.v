8e79a0ef5d1a08c2ddd9958f3c3e6bb846d3a3b9
Pratap Singh <pratapsingh1729@gmail.com>
2020-02-04 23:56:06 -0500

    added basic reflection for instrumentation, need to prove soundness

--- coq/lc_instrumentation.v
+++ coq/lc_instrumentation.v
@@ -275,14 +275,9 @@ Qed.
 Lemma not_valueb_sound : forall t, ~(value t) -> valueb t = false.
 Proof.
   intros.
-  destruct t; simpl; try reflexivity; try contradict H.
-  - apply (v_abs s t).
-  - apply (v_nat n).
-  - apply (v_unit).
-  - apply (v_loc n).
+  destruct t; simpl; try reflexivity; try contradict H; constructor.
 Qed.
-
-
+  
 Theorem stepfn_sound : forall t1 s1 t2 s2,
     t1 / s1 --> t2 / s2 -> stepfn t1 s1 = (t2, s2).
 Proof.
@@ -292,10 +287,10 @@ Proof.
     rewrite H1.
     simpl.
     reflexivity.
-  - specialize (not_valueb_sound t1_1).
-    specialize (step_not_value t1_1 s1 t1' s2 H1) as NV.
-    specialize (H2 NV).
-    rewrite H2.
+  - specialize (not_valueb_sound t1_1) as VBS;
+    specialize (step_not_value t1_1 s1 t1' s2 H1) as NV;
+    specialize (VBS NV);
+    rewrite VBS.
     specialize (IHt1_1 s1 t1' s2).
     rewrite IHt1_1.
     reflexivity.
@@ -387,3 +382,247 @@ Proof.
     rewrite IHt1_2.
     reflexivity.
 Qed.
+
+
+(* --------------------------------------------------- *)
+(* META LANGUAGE - add counters to stores, incremented each time
+   the loc is refed, derefed, or assigned *)
+
+
+Inductive mtm : Type :=
+  | mvar    : string -> mtm
+  | mapp    : mtm -> mtm -> mtm
+  | mabs    : string -> mtm -> mtm
+  | mconst  : nat -> mtm
+  | mscc    : mtm -> mtm
+  | mprd    : mtm -> mtm
+  | mtest0  : mtm -> mtm -> mtm -> mtm
+  | munit   : mtm
+  | mref    : mtm -> mtm
+  | mderef  : mtm -> mtm
+  | massign : mtm -> mtm -> mtm
+  | mloc    : nat -> mtm
+  | merror  : string -> mtm.
+
+(* values *)
+Inductive mvalue : mtm -> Prop :=
+  | mv_abs  : forall x t,
+      mvalue (mabs x t)
+  | mv_nat : forall n,
+      mvalue (mconst n)
+  | mv_unit :
+      mvalue munit
+  | mv_loc : forall l,
+      mvalue (mloc l).
+Hint Constructors mvalue.
+
+Fixpoint mvalueb (mt : mtm) : bool :=
+  match mt with
+  | mabs _ _ | mconst _ | munit | mloc _ => true
+  | _ => false
+  end.
+
+Theorem mvalueb_sound : forall mt, mvalue mt -> (mvalueb mt = true).
+Proof.
+  intros.
+  inversion H; simpl; reflexivity.
+Qed.
+
+Theorem mvalueb_complete : forall mt, (mvalueb mt = true) -> mvalue mt.
+Proof.
+  intros.
+  destruct mt; simpl; try discriminate; auto.
+Qed.
+
+(* substitution. NB ignores free-variable issues. *)
+Fixpoint msubst (x:string) (s:mtm) (t:mtm) : mtm :=
+  match t with
+  | mvar x'       =>
+      if String.eqb x x' then s else t
+  | mapp t1 t2    =>
+      mapp (msubst x s t1) (msubst x s t2)
+  | mabs x' t1  =>
+      if String.eqb x x' then t else mabs x' (msubst x s t1)
+  | mconst n        =>
+      t
+  | mscc t1      =>
+      mscc (msubst x s t1)
+  | mprd t1      =>
+      mprd (msubst x s t1)
+  | mtest0 t1 t2 t3 =>
+      mtest0 (msubst x s t1) (msubst x s t2) (msubst x s t3)
+  | munit         =>
+      t
+  | mref t1       =>
+      mref (msubst x s t1)
+  | mderef t1     =>
+      mderef (msubst x s t1)
+  | massign t1 t2 =>
+      massign (msubst x s t1) (msubst x s t2)
+  | mloc _        =>
+      t
+  | merror _      => t 
+  end.
+
+
+Definition mstore := list (mtm * nat).
+
+Definition mstore_lookup (n:nat) (mst:mstore) :=
+  match nth n mst (munit, O) with
+  | (mt, _) => mt
+  end.
+
+Definition mstore_incr_loc (n:nat) (mst:mstore) :=
+  match nth n mst (munit, O) with
+  | (mt, ct) => replace n (mt, S ct) mst
+  end. 
+
+Definition replace_incr_loc (n:nat) (t:mtm) (mst:mstore) :=
+  match nth n mst (munit, O) with
+  | (_, ct) => replace n (t, S ct) mst
+  end. 
+
+Fixpoint mstepfn (t : mtm) (s : mstore) : mtm * mstore :=
+  match t with
+  | mvar _ | munit | mconst _ | mabs _ _ | mloc _ | merror _ => (t,s)
+  | mapp fn arg =>
+    if (mvalueb fn) then
+      if (mvalueb arg) then
+        match fn with
+        | mabs x body => (msubst x arg body, s)
+        | _ => (merror "attempt to apply non-fn", s)
+        end
+      else
+        let (arg',s') := mstepfn arg s in
+        (mapp fn arg', s')
+    else
+      let (fn',s') := mstepfn fn s in
+      (mapp fn' arg, s')
+  | mscc t1 =>
+    if (mvalueb t1) then
+      match t1 with
+      | mconst n => (mconst (S n), s)
+      | _ => (merror "scc of non nat", s)
+      end
+    else
+      let (t1', s') := mstepfn t1 s in
+      (mscc t1', s')
+  | mprd t1 =>
+    if (mvalueb t1) then
+      match t1 with
+      | mconst n => (mconst (pred n), s)
+      | _ => (merror "prd of non nat", s)
+      end
+    else
+      let (t1', s') := mstepfn t1 s in
+      (mprd t1', s')
+  | mtest0 cond yes no =>
+    if (mvalueb cond) then
+      match cond with
+      | mconst 0 => (yes, s)
+      | mconst (S n) => (no, s)
+      | _ => (merror "test0 of non nat", s)
+      end
+    else
+      let (cond', s') := mstepfn cond s in
+      (mtest0 cond' yes no, s')
+  (* Add instrumentation to the store *)
+  | mref t1 =>
+    if (mvalueb t1) then
+      (mloc (length s), (s ++ (t1,0)::nil))
+    else
+      let (t1', s') := mstepfn t1 s in
+      (mref t1', s')
+  | mderef t1 =>
+    if (mvalueb t1) then
+      match t1 with
+      | mloc l =>
+        if Nat.ltb l (length s) then
+          (mstore_lookup l s, mstore_incr_loc l s)
+        else (merror "attempt to deref invalid loc", s)
+      | _ => (merror "attempt to deref non-loc", s)
+      end
+    else
+      let (t1', s') := mstepfn t1 s in
+      (mderef t1', s')
+  | massign t1 t2 =>
+    if (mvalueb t1) then
+      if (mvalueb t2) then
+        match t1 with
+        | mloc l =>
+          if Nat.ltb l (length s) then
+            (munit, replace_incr_loc l t2 s)
+          else (merror "attempt to assign invalid loc", s)
+        | _ => (merror "attempt to assign non-loc", s)
+        end
+      else
+        let (t2', s') := mstepfn t2 s in
+        (massign t1 t2', s')
+    else
+      let (t1', s') := mstepfn t1 s in
+      (massign t1' t2, s')   
+  end.
+
+Fixpoint reify_tm (t : tm) : mtm :=
+  match t with
+  | var x' => mvar x'
+  | app t1 t2 => mapp (reify_tm t1) (reify_tm t2)
+  | abs x' t1 => mabs x' (reify_tm t1)
+  | const n => mconst n
+  | scc t1 => mscc (reify_tm t1)
+  | prd t1 => mprd (reify_tm t1)
+  | test0 t1 t2 t3 => mtest0 (reify_tm t1) (reify_tm t2) (reify_tm t3)
+  | unit => munit
+  | ref t1 => mref (reify_tm t1)
+  | deref t1  => mderef (reify_tm t1)
+  | assign t1 t2 => massign (reify_tm t1) (reify_tm t2)
+  | loc l => mloc l
+  | error s => merror s 
+  end.
+
+Definition reify_st (st : store) : mstore :=
+  map (fun t => (reify_tm t, O)) st.
+ 
+Definition reify (t : tm) (st : store) : (mtm * mstore) :=
+  (reify_tm t, reify_st st).
+
+Fixpoint reflect_mtm (mt : mtm) : tm :=
+  match mt with
+  | mvar x' => var x'
+  | mapp t1 t2 => app (reflect_mtm t1) (reflect_mtm t2)
+  | mabs x' t1 => abs x' (reflect_mtm t1)
+  | mconst n => const n
+  | mscc t1 => scc (reflect_mtm t1)
+  | mprd t1 => prd (reflect_mtm t1)
+  | mtest0 t1 t2 t3 => test0 (reflect_mtm t1) (reflect_mtm t2) (reflect_mtm t3)
+  | munit => unit
+  | mref t1 => ref (reflect_mtm t1)
+  | mderef t1 => deref (reflect_mtm t1)
+  | massign t1 t2 => assign (reflect_mtm t1) (reflect_mtm t2)
+  | mloc l => loc l
+  | merror s => error s 
+  end.
+
+Definition reflect_mst (mst : mstore) : store :=
+  map (fun pair =>
+         match pair with
+         | (t, _) => reflect_mtm t
+         end)
+      mst.
+
+Definition reflect (mt : mtm) (mst : mstore) : (tm * store) :=
+  (reflect_mtm mt, reflect_mst mst).
+
+Theorem reflection_sound :
+  forall t s t' s' mt ms mt' ms',
+    stepfn t s = (t', s') ->
+    reify t s = (mt, ms) ->
+    mstepfn mt ms = (mt', ms') ->
+    reflect mt' ms' = (t', s').
+Proof.
+  intros.
+Abort.
+
+
+                   
+
